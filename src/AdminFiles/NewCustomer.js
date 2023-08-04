c9671d9bab56aa279669d6b5c0dbd7262543c950
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-08-04 16:08:48 +0530

    Update NewCustomer.js
    - handled clicks
    - added placeholders
    - state variables for inputs

--- src/AdminFiles/NewCustomer.js
+++ src/AdminFiles/NewCustomer.js
@@ -1,45 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function NewCustomer() {
   const navigate = useNavigate();
-  const handleDoneClick = () => {
+  //for details of customer
+  const initialCustomerDetails = {
+    CompanyName: "",
+    Email: "",
+    Password: "",
+  };
+  const [customerDetails, setcustomerDetails] = useState(
+    initialCustomerDetails
+  );
+  const handleCustomerDetailsChange = (event) => {
+    setcustomerDetails({
+      ...customerDetails,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleDoneClick = (event) => {
+    event.preventDefault();
+    console.log(customerDetails);
     navigate("/adminDashboard");
   };
-  const handleCreateBuildingClick = () => {
+  const handleCreateBuildingClick = (event) => {
+    event.preventDefault();
+    handleDoneClick(event);
     navigate("/adminDashboard/newBuildingForm");
   };
   return (
     <div className="container">
       <form>
         <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">
+          <label htmlFor="exampleInputEmail1" className="form-label">
             Name of the company
           </label>
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            placeholder="clairco"
+            name="CompanyName"
+            value={customerDetails.CompanyName}
+            onChange={handleCustomerDetailsChange}
+          />
         </div>
         <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">
+          <label htmlFor="exampleInputEmail1" className="form-label">
             Email address
           </label>
           <input
             type="email"
             className="form-control"
+            placeholder="clair@gmail.com"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            name="Email"
+            value={customerDetails.Email}
+            onChange={handleCustomerDetailsChange}
           />
         </div>
         <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">
+          <label htmlFor="exampleInputPassword1" className="form-label">
             Password
           </label>
           <input
             type="password"
             className="form-control"
             id="exampleInputPassword1"
+            placeholder="123@saymyname"
+            name="Password"
+            value={customerDetails.Password}
+            onChange={handleCustomerDetailsChange}
           />
         </div>
-
         <div className="d-inline">
           <button
             type="submit"
@@ -50,7 +83,7 @@ export default function NewCustomer() {
           </button>
           <button
             type="button"
-            class="btn btn-success m-1"
+            className="btn btn-success m-1"
             onClick={handleCreateBuildingClick}
           >
             Create Building
@@ -59,11 +92,11 @@ export default function NewCustomer() {
               width="16"
               height="16"
               fill="currentColor"
-              class="bi bi-arrow-right"
+              className="bi bi-arrow-right"
               viewBox="0 0 16 16"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
               ></path>
             </svg>
