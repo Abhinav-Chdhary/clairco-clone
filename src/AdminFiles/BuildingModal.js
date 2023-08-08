7124a5e3a00cbdc86cd8f27e55599ed8889ff640
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-08-08 15:52:56 +0530

    Update BuildingModal.js
    - Feature to edit details of Building on frontEnd.

--- src/AdminFiles/BuildingModal.js
+++ src/AdminFiles/BuildingModal.js
@@ -5,24 +5,43 @@ import { useState } from "react";
 
 export default function BuildingModal({ openModal, ...buildingProps }) {
   let initialBuildingDetails = {
-    ...buildingProps,
-    has_device: buildingProps.has_device ? "Yes" : "No",
-    pm_2_5: buildingProps.pm_2_5 ? "Yes" : "No",
-    pm_10: buildingProps.pm_10 ? "Yes" : "No",
-    co2: buildingProps.co2 ? "Yes" : "No",
-    tvoc: buildingProps.tvoc ? "Yes" : "No",
-    temperature: buildingProps.temperature ? "Yes" : "No",
-    humidity: buildingProps.humidity ? "Yes" : "No",
+    company: buildingProps.company,
+    building: buildingProps.building,
+    geolocation: buildingProps.geolocation,
+    address: buildingProps.address,
+    area: buildingProps.area,
+    has_device: buildingProps.has_device,
+    pm_2_5: buildingProps.pm_2_5,
+    pm_10: buildingProps.pm_10,
+    co2: buildingProps.co2,
+    tvoc: buildingProps.tvoc,
+    temperature: buildingProps.temperature,
+    humidity: buildingProps.humidity,
   };
 
   const [buildingDetails, setBuildingDetails] = useState(
     initialBuildingDetails
   );
+  //input change
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+
+    setBuildingDetails((prevDetails) => ({
+      ...prevDetails,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+  //submit changes
+  const saveChanges = async (event) => {
+    event.preventDefault();
+    console.log(buildingDetails);
+  };
+
   return (
     <div>
       <Modal
         show={true}
-        onClick={() => {
+        onHide={() => {
           openModal(false);
         }}
       >
@@ -33,7 +52,18 @@ export default function BuildingModal({ openModal, ...buildingProps }) {
           <div className="building-details">
             {Object.entries(initialBuildingDetails).map(([key, value]) => (
               <div key={key}>
-                <strong>{key.replace(/_/g, " ")}</strong>: {value}
+                <strong>{key.replace(/_/g, " ")}</strong>:{" "}
+                {typeof value === "boolean" ? (
+                  <input
+                    type="checkbox"
+                    checked={buildingDetails[key]}
+                    name={key}
+                    value={buildingDetails[key]}
+                    onChange={handleChange}
+                  />
+                ) : (
+                  <span>{value}</span>
+                )}
               </div>
             ))}
           </div>
@@ -47,12 +77,7 @@ export default function BuildingModal({ openModal, ...buildingProps }) {
           >
             Close
           </Button>
-          <Button
-            variant="primary"
-            onClick={() => {
-              openModal(false);
-            }}
-          >
+          <Button variant="primary" onClick={saveChanges}>
             Save Changes
           </Button>
         </Modal.Footer>
