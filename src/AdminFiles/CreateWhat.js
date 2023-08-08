bc501b57df9374097c9be55d52924b4f78f4b4ae
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-08-08 17:47:41 +0530

    Update CreateWhat.js
    - Go back feature improvement

--- src/AdminFiles/CreateWhat.js
+++ src/AdminFiles/CreateWhat.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function CreateWhat() {
+  let navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
     <div className="d-flex flex-column">
       <div>
-        <Link
+        <Button
           className="btn btn-danger btn-lg ms-3"
-          to="/adminDashboard"
+          onClick={goBack}
           role="button"
         >
           Back
-        </Link>
+        </Button>
       </div>
       <div className="container d-flex justify-content-evenly">
         <div className="zoom shadow-lg p-3 mb-5 bg-body rounded d-flex flex-column border text-center rounded">
