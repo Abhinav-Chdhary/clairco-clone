1d32e8eb5c9f6c1b124590fa27dea247e54ccc37
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-08-08 13:46:54 +0530

    Update BuildingModal.js
    - Successfully able to display all details of the building

--- src/AdminFiles/BuildingModal.js
+++ src/AdminFiles/BuildingModal.js
@@ -4,7 +4,16 @@ import Modal from "react-bootstrap/Modal";
 import { useState } from "react";
 
 export default function BuildingModal({ openModal, ...buildingProps }) {
-  let initialBuildingDetails = { ...buildingProps };
+  let initialBuildingDetails = {
+    ...buildingProps,
+    has_device: buildingProps.has_device ? "Yes" : "No",
+    pm_2_5: buildingProps.pm_2_5 ? "Yes" : "No",
+    pm_10: buildingProps.pm_10 ? "Yes" : "No",
+    co2: buildingProps.co2 ? "Yes" : "No",
+    tvoc: buildingProps.tvoc ? "Yes" : "No",
+    temperature: buildingProps.temperature ? "Yes" : "No",
+    humidity: buildingProps.humidity ? "Yes" : "No",
+  };
 
   const [buildingDetails, setBuildingDetails] = useState(
     initialBuildingDetails
@@ -21,7 +30,13 @@ export default function BuildingModal({ openModal, ...buildingProps }) {
           <Modal.Title>{buildingDetails.building}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div>{buildingDetails.building}</div>
+          <div className="building-details">
+            {Object.entries(initialBuildingDetails).map(([key, value]) => (
+              <div key={key}>
+                <strong>{key.replace(/_/g, " ")}</strong>: {value}
+              </div>
+            ))}
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button
@@ -45,3 +60,17 @@ export default function BuildingModal({ openModal, ...buildingProps }) {
     </div>
   );
 }
+/*let initialBuildingDetails = {
+    company: buildingProps.company,
+    building: buildingProps.building,
+    geolocation: buildingProps.geolocation,
+    address: buildingProps.address,
+    area: buildingProps.area,
+    has_device: buildingProps.has_device,
+    pm_2_5: buildingProps.pm_2_5,
+    pm_10: buildingProps.pm_10,
+    co2: buildingProps.co2,
+    tvoc: buildingProps.tvoc,
+    temperature: buildingProps.temperature,
+    humidity: buildingProps.humidity,
+  };*/
