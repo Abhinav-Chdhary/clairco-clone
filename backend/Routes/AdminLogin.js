49dbbc057214f1587a57a44da388026857276a3d
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-08-09 16:02:38 +0530

    Update AdminLogin.js
    - removed unnecessary imports

--- backend/Routes/AdminLogin.js
+++ backend/Routes/AdminLogin.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const AdminUser = require("../Models/AdminUser");
 const { body, validationResult } = require("express-validator");
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = "tfuhtlpzhiopuhchukaopzdlizpalpzn";
 
