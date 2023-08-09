d1ff3ce397d29ad739a4202963cc76c571f05e38
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-08-09 15:49:08 +0530

    Delete Customer feature
    - Now able to delete buildings associated with the company and the company
    - and tested

--- backend/Routes/AdminDeleteCustomer.js
+++ backend/Routes/AdminDeleteCustomer.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const Customer = require("../Models/Customer");
+const Building = require("../Models/Building");
 
 router.delete("/deleteCustomer", async (req, res) => {
   try {
     const { id, company } = req.body;
-    //console.log(id);
+    //delete all buildings associated with the company
+    const deleteBuilding = await Building.deleteMany({ company: company });
+    //delete the company
     const deleteCompany = await Customer.findByIdAndDelete(id);
     if (!deleteCompany) {
       return res
