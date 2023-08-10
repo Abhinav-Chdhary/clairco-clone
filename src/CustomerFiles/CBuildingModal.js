b193ab3fc0a00b92c9515f56d238d84023cf5197
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-08-10 14:57:54 +0530

    Update CBuildingModal.js
    - better control over building details

--- src/CustomerFiles/CBuildingModal.js
+++ src/CustomerFiles/CBuildingModal.js
@@ -2,7 +2,21 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-export default function CBuildingModal({ openModal, ...buildingDetails }) {
+export default function CBuildingModal({ openModal, ...buildingProps }) {
+  let buildingDetails = {
+    company: buildingProps.company,
+    building: buildingProps.building,
+    geolocation: buildingProps.geolocation,
+    address: buildingProps.address,
+    area: buildingProps.area,
+    has_device: buildingProps.has_device,
+    pm_2_5: buildingProps.pm_2_5,
+    pm_10: buildingProps.pm_10,
+    co2: buildingProps.co2,
+    tvoc: buildingProps.tvoc,
+    temperature: buildingProps.temperature,
+    humidity: buildingProps.humidity,
+  };
   return (
     <div>
       <Modal
