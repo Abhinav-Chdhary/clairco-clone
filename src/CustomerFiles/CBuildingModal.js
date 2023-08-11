8a63825934127b61c12fb7b9528b4d75e8b6399f
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-08-11 13:24:35 +0530

    AQI view feature
    - Update data every 60 seconds
    - View data

--- src/CustomerFiles/CBuildingModal.js
+++ src/CustomerFiles/CBuildingModal.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
@@ -18,31 +18,43 @@ export default function CBuildingModal({ openModal, ...buildingProps }) {
     humidity: buildingProps.humidity,
   };
   const latLong = buildingDetails.geolocation.split(",");
+  const [displayData, setDisplayData] = useState({});
   //format is DD
   let lat = parseFloat(latLong[0]);
   let lon = parseFloat(latLong[1].trim());
-  const loadData = async () => {
-    try {
-      let response = await fetch(
-        `https://air-quality-by-api-ninjas.p.rapidapi.com/v1/airquality?lat=${lat}&lon=${lon}`,
-        {
-          method: "GET",
-          headers: {
-            "X-RapidAPI-Key":
-              "96c6d1bb3emsh36e1c6665ba1936p1902bdjsn80a504f557e1",
-            "X-RapidAPI-Host": "air-quality-by-api-ninjas.p.rapidapi.com",
-          },
-        }
-      );
-      response = await response.json();
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   useEffect(() => {
+    const loadData = async () => {
+      try {
+        let response = await fetch(
+          `https://air-quality-by-api-ninjas.p.rapidapi.com/v1/airquality?lat=${lat}&lon=${lon}`,
+          {
+            method: "GET",
+            headers: {
+              "X-RapidAPI-Key":
+                "96c6d1bb3emsh36e1c6665ba1936p1902bdjsn80a504f557e1",
+              "X-RapidAPI-Host": "air-quality-by-api-ninjas.p.rapidapi.com",
+            },
+          }
+        )
+          .then((response) => response.json())
+          .then((displayData) => setDisplayData(displayData))
+          .catch((error) => console.error("Error fetching data:", error));
+        //response = await response.json();
+        // console.log(response.CO);
+      } catch (error) {
+        console.log(error);
+      }
+    };
     loadData();
+    const intervalId = setInterval(() => {
+      console.log("I loaded");
+      loadData();
+    }, 60000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
 
   return (
@@ -54,10 +66,7 @@ export default function CBuildingModal({ openModal, ...buildingProps }) {
         }}
       >
         <Modal.Header closeButton>
-          <Modal.Title>
-            {buildingDetails.building}
-            {console.log(latLong[0] + "-" + latLong[1].trim())}
-          </Modal.Title>
+          <Modal.Title>{buildingDetails.building}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div className="building-details">
@@ -66,6 +75,22 @@ export default function CBuildingModal({ openModal, ...buildingProps }) {
                 <strong>{key.replace(/_/g, " ")}</strong>: <span>{value}</span>
               </div>
             ))}
+          </div>
+          {buildingDetails.has_device ? <div>YES</div> : <div>NO</div>}
+          <div>
+            {buildingDetails.has_device ? (
+              displayData &&
+              Object.keys(displayData).map((key) => (
+                <div key={key}>
+                  <p>
+                    {key} Conc: {displayData[key].concentration}
+                  </p>
+                  {/* <p>AQI: {displayData[key].aqi}</p> */}
+                </div>
+              ))
+            ) : (
+              <div>....</div>
+            )}
           </div>
         </Modal.Body>
         <Modal.Footer>
